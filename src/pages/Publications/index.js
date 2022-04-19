b09cbefc96297dc9e4ad74113471712c6f23a420
Anne L'Hôte <anne.lhote@gmail.com>
2022-04-19 17:49:26 +0200

    feat(publications): Add search bar on publications title

--- src/pages/Publications/index.js
+++ src/pages/Publications/index.js
@@ -1,4 +1,4 @@
-import { Elasticsearch, Facet, QueryBuilder, Results, SearchBox } from 'react-elasticsearch';
+import { Elasticsearch, Facet, Results, SearchBox } from 'react-elasticsearch';
 import { Card, CardDescription, CardDetail, CardTitle } from '@dataesr/react-dsfr';
 
 import './index.css'
@@ -16,17 +16,11 @@ const MyCardItem = (props) => {
 const Publications = () => {
     return (
         <Elasticsearch url="http://localhost:9200/scanr-backoffice">
-            {/* <QueryBuilder
-                id="query_builder"
-                fields={[
-                    { value: "title.keyword", text: "Title" }
-                ]}
-            /> */}
             <Facet
                 id="detected_countries"
                 fields={['detected_countries.keyword']}
             />
-            <SearchBox id="mainSearch" />
+            <SearchBox id="mainSearch" fields={["title"]}/>
             <Results
                 id="results"
                 items={data =>
